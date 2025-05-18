cd054fb78fb5faa365b91aca59f9b63b30b03575
ashokide <kbnashok@gmail.com>
2025-05-19 02:12:54 +0530

    changes on sum method

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# This is a simple Python script that defines a function to sum two numbers
+# and prints the result of calling that function with two arguments.
 def sum(value1, value2):
     """
     This function takes two values and returns their sum.
